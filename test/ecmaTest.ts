b6485d4e153ead1d66ea60899b07edaa447e3e93
Takeshi Kurosawa <taken.spc@gmail.com>
2016-02-18 22:55:32 +0900

    Name oldURLs to urls in ecmaTest.ts

--- test/ecmaTest.ts
+++ test/ecmaTest.ts
@@ -6,17 +6,20 @@ import * as ecma from '../rules/ecma';
 //
 describe('ecma', () => {
     it('should normalize urls of ECMA-262 5.1', () => {
-        const oldURLs = [
+        const urls = [
             'http://ecma-international.org/ecma-262/5.1/index.html',
             'http://ecma-international.org/ecma-262/5.1/',
             'http://www.ecma-international.org/ecma-262/5.1/index.html',
             'http://www.ecma-international.org/ecma-262/5.1/',
         ];
-        
+
         const expected = 'http://www.ecma-international.org/ecma-262/5.1/';
 
-        for (let i = 0; i < oldURLs.length; i++) {
-            const actual = ecma.normalize(url.parse(oldURLs[i]), true);
+        for (let i = 0; i < urls.length; i++) {
+            const actual = ecma.normalize(url.parse(urls[i]), true);
+            assert(actual === expected);
+        }
+    });
 
     it('should normalize urls of ECMA-262 6.0 draft', () => {
         const urls = [
@@ -33,18 +36,18 @@ describe('ecma', () => {
     });
 
     it('should normalize urls of ECMA-262 6.0', () => {
-        const oldURLs = [
+        const urls = [
             'http://ecma-international.org/ecma-262/6.0/index.html',
             'http://ecma-international.org/ecma-262/6.0/',
             'http://www.ecma-international.org/ecma-262/6.0/index.html',
             'http://www.ecma-international.org/ecma-262/6.0/',
 
         ];
-        
+
         const expected = 'http://www.ecma-international.org/ecma-262/6.0/';
 
-        for (let i = 0; i < oldURLs.length; i++) {
-            const actual = ecma.normalize(url.parse(oldURLs[i]), true);
+        for (let i = 0; i < urls.length; i++) {
+            const actual = ecma.normalize(url.parse(urls[i]), true);
             assert(actual === expected);
         }
     });
